b2053fd16dd8b2d49005ec78725be046c5682295
Joaquin <joaquinyapura@gmail.com>
2025-04-03 10:30:57 -0300

    sticky nav

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 export const NavBar = () => {
   return (
-    <nav className="w-full p-4 flex justify-center text-white font-bayon  md:text-3xl sticky">
+    <nav className="w-full p-4 flex justify-center text-white bg-gray-700/10 backdrop-blur-sm   font-bayon  md:text-3xl sticky top-0">
       <ul className="flex gap-6">
         <Link to={"/"}>HOME</Link>
         <Link to={"/contact"}>CONTACT</Link>
